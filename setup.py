c53d4af111378adab4b93728728980d1fe7fac4c
VallyWonka <55382104+VallyWonka@users.noreply.github.com>
2020-03-04 09:28:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-TOKEN = ''  # insert your token here
+TOKEN = '1129152192:AAFfolPZVQAUJ_7jYDHy0DOX2exSwhb40Gs'  # insert your token here
 PROXY = 'https://telegg.ru/orig/bot'  # proxy to connect against telegram ban
