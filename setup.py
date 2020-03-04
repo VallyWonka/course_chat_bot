e543739932f30a40d54952e4a2bd56aa929f3551
VallyWonka <55382104+VallyWonka@users.noreply.github.com>
2020-03-04 09:47:57 +0300

    Revert "Update setup.py"
    This reverts commit c53d4af111378adab4b93728728980d1fe7fac4c.

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-TOKEN = '1129152192:AAFfolPZVQAUJ_7jYDHy0DOX2exSwhb40Gs'  # insert your token here
+TOKEN = ''  # insert your token here
 PROXY = 'https://telegg.ru/orig/bot'  # proxy to connect against telegram ban
